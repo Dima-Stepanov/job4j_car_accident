67f7f61f8bed7e47515c07590687eadf3bbb748c
dmitryjob4j <haoos@inbox.ru>
2022-06-09 11:43:34 +0300

    create project

---
+++ src/main/java/ru/job4j/accident/CarAccidentApplication.java
@@ -0,0 +1,15 @@
+package ru.job4j.accident;
+
+/**
+ * 3. Мидл
+ * 3.4. Spring
+ * 3.4.0. Проект. "Автонарушители".
+ *
+ * @author Dmitry Stepanov, user Dmitry
+ * @since 09.06.2022
+ */
+public class CarAccidentApplication {
+    public static void main(String[] args) {
+
+    }
+}
