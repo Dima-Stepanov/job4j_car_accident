c6c0e0a4c8d560fbd07953905d1ef01b139dccbe
dmitryjob4j <haoos@inbox.ru>
2022-07-04 00:46:01 +0300

    Spring security

---
+++ src/main/java/ru/job4j/accident/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package ru.job4j.accident.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation
+        .web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * 3. Мидл
+ * 3.4. Spring
+ * 0. Spring Security [#6879]
+ * SecurityConfig Конфигурирование Spring.
+ * Создадим отдельный класс, в котором сделаем настройки для авторизации.
+ * В этом уроке пользователи будут храниться в памяти.
+ *
+ * @author Dmitry Stepanov, user Dima_Nout
+ * @since 03.07.2022
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder passwordEncoder =
+                PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder)
+                .withUser("user")
+                .password(passwordEncoder.encode("123456"))
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password(passwordEncoder.encode("123456"))
+                .roles("USER", "ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/login")
+                .permitAll()
+                .antMatchers("/**")
+                .hasAnyRole("ADMIN", "USER")
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .failureUrl("/login?error=true")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login?logout=true")
+                .invalidateHttpSession(true)
+                .permitAll()
+                .and()
+                .csrf()
+                .disable();
+    }
+}
---
+++ src/main/java/ru/job4j/accident/init/SecurityInit.java
@@ -0,0 +1,18 @@
+package ru.job4j.accident.init;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * 3. Мидл
+ * 3.4. Spring
+ * 3.4.4. Security
+ * 0. Spring Security [#6879]
+ * SecurityInit подключение фильтров.
+ * Он подключает DelegatingFilterProxy.
+ * Это главный фильтр, в котором идет обработка запросов.
+ *
+ * @author Dmitry Stepanov, user Dima_Nout
+ * @since 03.07.2022
+ */
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
